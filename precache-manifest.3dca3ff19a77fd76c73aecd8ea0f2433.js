4cecf488a8680662cc68410f7d5a46ca36ba9adb
Alex Costigan <alexcostigan@icloud.com>
2020-03-29 20:26:38 +0100

    Updates

---
+++ precache-manifest.3dca3ff19a77fd76c73aecd8ea0f2433.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1662df6432abf3e0d333e3cd11e43bf3",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "1e049359d035c01b3a75",
+    "url": "/website/static/css/main.d06dd990.chunk.css"
+  },
+  {
+    "revision": "dde90359fd96d60f8c94",
+    "url": "/website/static/js/2.7c679ea7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/website/static/js/2.7c679ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e049359d035c01b3a75",
+    "url": "/website/static/js/main.f9f8bc9d.chunk.js"
+  },
+  {
+    "revision": "af32a2331a335e5f4319",
+    "url": "/website/static/js/runtime-main.dcf900d9.js"
+  },
+  {
+    "revision": "c27ef03849b9033906c6e36d4f39d008",
+    "url": "/website/static/media/Alex.c27ef038.png"
+  },
+  {
+    "revision": "e8218da5f0f86b099815af2065d71419",
+    "url": "/website/static/media/One.e8218da5.png"
+  },
+  {
+    "revision": "2591304a5e16982174cfbd4ee432df52",
+    "url": "/website/static/media/Photo.2591304a.jpg"
+  }
+]);
